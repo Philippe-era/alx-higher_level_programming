13a18072c786e7a6473bdb51acecec120e9a2cb3
= <=>
2023-06-05 04:05:22 -0700

    Fixed line 8

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -3,4 +3,3 @@ phrase = "Python is an interpreted, interactive, object-oriented programming
         language that combines remarkable power with very clear syntax"
 phrase = phrase[39:67] + phrase[107:112] + phrase[:6]
 print(phrase)
-
