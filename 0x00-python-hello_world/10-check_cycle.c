4bda1cc197658bb34545372693f99bc8814f32c1
= <=>
2023-06-05 15:28:07 -0700

    final error fixed

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -3,26 +3,25 @@
 /**
  * check_cycle - checks if a linked list contains a cycle
  * @list: list to be analyzed
- *
  * Return: 1 is success otherwise fail
  */
 int check_cycle(listint_t *list)
 {
 	listint_t *first_check = list;
 	listint_t *last_check = list;
-	int fail = 0, success = 1;
+/*	int fail = 0, success = 1; */
 
 	if (!list)
-		return (fail);
+		return (1);
 
 	while (first_check && last_check && last_check->next_node)
 	{
 		first_check = first_check->next_node;
 		last_check = last_check->next_node->next_node;
 		if (first_check == last_check)
-			return (success);
+			return (0);
 	}
 
-	return (fail);
+	return (1);
 }
 
