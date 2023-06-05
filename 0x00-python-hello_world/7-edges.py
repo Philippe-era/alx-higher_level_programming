b1dd7eba991b75023eda8467f31ec6843157f76b
= <=>
2023-06-05 02:56:12 -0700

    Task 7

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+phrase = "Holberton"
+phrase_third = phrase[0:3]
+phrase_second = phrase[-2:]
+phrase_middle = phrase[1:-1]
+print("First 3 letters: {}".format(phrase_third))
+print("Last 2 letters: {}".format(phrase_second))
+print("Middle word: {}".format(phrase_middle))
+
