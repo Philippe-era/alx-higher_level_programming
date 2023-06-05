5e3b8226b966429b1b60aee8f9fa3597704c4c7f
= <=>
2023-06-05 03:44:38 -0700

    fixed concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-first_num = 39
-second_num = 67
-third_num = 107
-fourth_num = 112
-last_num = 6
 phrase = "Python is an interpreted, interactive, object-oriented programming\
         language that combines remarkable power with very clear syntax"
-phrase = phrase[first_num:second_num] + phrase[third_num:fourth_num] + phrase[0:last_num]
+phrase = phrase[39:67] + phrase[107:112] + phrase[:6]
 print(phrase)
 
