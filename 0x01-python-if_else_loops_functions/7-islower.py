a2f64477358c5b19e3b4b491f2bffc6092817128
= <=>
2023-06-06 02:49:52 -0700

    Task 8 change code a bit

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,11 @@
+
+#!/usr/bin/python3
+first_num = 97
+second_num = 122
+
+def islower(c):
+    if ord(c) >= first_num and ord(c) <= second_num:
+        return True
+    else:
+        return False
+
