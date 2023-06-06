d878bd165b90586476bd95aa12193df916757135
= <=>
2023-06-06 11:25:02 -0700

    final task 11

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 zero_check = 0
-add_num = 1
+
 def remove_char_at(str, n):
         if n < zero_check:
         return (str)
-    return (str[:n] + str[n+add_num:])
-
+    return (str[:n] + str[n + 1:])
