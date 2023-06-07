7a010e2699ef3339f08b7c3014dac263d70bd48c
= <=>
2023-06-07 01:35:43 -0700

    Final task fixed

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,33 +1,32 @@
 #include "lists.h"
 
 /**
- * insert_node – inserts a node in sorted linked list
- * @head: head pointer in linked list
- * @num: number that will be inputted
- *
- * Return: function fails null
+ * insert_node - number to be inputted in sorted linked list
+ * @head: header pointer
+ * @number: The number to be inputted
+ * Return: Null will be returned if function fails
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *node_check = *header, *node_check;
+	listint_t *node = *head, *node_check;
 
 	node_check = malloc(sizeof(listint_t));
 	if (node_check == NULL)
 		return (NULL);
 	node_check->num = number;
 
-	if (node == NULL || next_node->num>= number)
+	if (node == NULL || node->num >= number)
 	{
-		node_check->next = next_node;
-		*header = node_check;
+		node_check->next_node = node;
+		*head = node_check;
 		return (node_check);
 	}
 
-	while (next_node && next_node->next && next_node->next->num < number)
-		next_node = next_node->next;
+	while (node && node->next_node && node->next_node->num < number)
+		node = node->next_node;
 
-	node_check->next = next_node->next;
-	next_node->next = node_check;
+	node_check->next_node = node->next_node;
+	node->next_node = node_check;
 
 	return (node_check);
 }
