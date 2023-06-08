72d449a652646cf44222ece3c23ec84708626ac9
= <=>
2023-06-08 02:09:10 -0700

    fixed

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,8 +4,7 @@ if __name__ == "__main__":
     
     import hidden_4
 
-     hidden_module= dir(hidden_4)
-    for names in hidden_module:
-        if names[:2] != "__":
-            print(names)
-
+    hidden_module = dir(hidden_4)
+    for module in hidden_module:
+        if module[:2] != "__":
+            print(module)
