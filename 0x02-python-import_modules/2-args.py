78ef86f4f7c35e0aea5669351ec2f59d075c253a
= <=>
2023-06-08 08:29:07 -0700

    Task 2 solved

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,15 +2,16 @@
 
 if __name__ == "__main__":
     import sys
-    zero = 0
     one = 1
+    zero = 0
+
     count_num = len(sys.argv) - 1
     if count_num == zero:
         print("0 arguments.")
     elif count_num == one:
         print("1 argument:")
     else:
-        print("{} arguments:".format(count))
+        print("{} arguments:".format(count_num))
     for initial in range(count_num):
         print("{}: {}".format(initial + 1, sys.argv[initial + 1]))
 
