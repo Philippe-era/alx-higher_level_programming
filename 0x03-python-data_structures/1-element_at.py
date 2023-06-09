8afbfba7ba70cc3e83957202107d9a58ae3c38c1
= <=>
2023-06-09 02:12:40 -0700

    First tasks

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+zero = 0
+
+def element_at(my_list, index):
+    if idx < zero or idx > (len(my_list) - 1):
+        return None
+    return (my_list[idx])
---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+zero = 0
+def replace_in_list(my_list, idx, element):
+    if idx >= zero and idx < len(my_list):
+        my_list[idx] = element
+    return (my_list)
+
