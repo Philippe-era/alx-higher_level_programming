d2d5b1740bd9af76db3fa4a82001157b14fae6f7
= <=>
2023-06-11 10:30:39 -0700

    solved

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-small_letter = ‘c’
-big_letter = ‘C’
+small_letter = 'c'
+big_letter = 'C'
 def no_c(my_string):
-        copy = [item for item in my_string if item != small_letter and item != big_letter]
-    return ("".join(copy))
+    string_check = ""
+    for initial in my_string:
+        if (initial != small_letter) and (initial != big_letter):
+            string_check += initial
+    return (string_check)
 
