ad853cac440a44c480807d3d29cd651c6e4d1bc6
= <=>
2023-06-12 02:52:09 -0700

    Fixed

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,26 @@
+#include <Python.h>
+
+/**
+ * print_python_list_info – lists to be printed
+ * @p: an object list
+ */
+void print_python_list_info(PyObject *p)
+{
+	int size_node, allocation_memory, initial;
+	PyObject *object_check;
+
+	size_node = Py_SIZE(p);
+	allocation_memory = ((PyListObject *)p)->allocated;
+
+	printf("[*] Size of the Python List = %d\n", size_node);
+	printf("[*] Allocated = %d\n", allocation_memory);
+
+	for (initial = 0; initial < size_node; initial++)
+	{
+		printf("Element %d: ", initial);
+
+		obj_check = PyList_GetItem(p, initial);
+		printf("%s\n", Py_TYPE(obj)->tp_name);
+	}
+}
+
--- 0x03-python-data_structures/linked_lists.c
+++ 0x03-python-data_structures/linked_lists.c
@@ -1,74 +1,75 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 
+
+  return (number);
+}
+
 /**
- * print_listint - prints all elements linked list
- * @h: header point
- * Return: nodes present will be displayed
+ * add_nodeint_end – node added to list 
+ * @head: header pointer
+ * @n: number of nodes
+ * Return: if added to list pass , otherwise fail
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+  listint_t *new_node;
+  listint_t *current_node;
+
+  current_node = *head;
+
+  new_node = malloc(sizeof(listint_t));
+  if (new_node == NULL)
+    return (NULL);
+
+  new_node->n = n;
+  new_node->next = NULL;
+
+  if (*head == NULL)
+    *head = new_node;
+  else
+    {
+      while (current_node->next != NULL)
+	current_node = current_node->next;
+      current_node->next = new_node;
+    }
+
+  return (new_node);
+}
+
+/**
+ * print_listint – all elements to be printed
+ * @h: header pointer
+ * Return: counts numbers of nodes
  */
 size_t print_listint(const listint_t *h)
 {
-	const listint_t *current_node;
-	unsigned int num;
+  const listint_t *current_node;
+  unsigned int number;
 
-	current_node = h;
-	num = 0;
-
-	while (current_node != NULL)
-	{
-		printf("%i\n", current_node->num);
-		current_node = current_node->next_node;
-		num++;
-	}
-	return (num);
-}
+  current_node = h;
+  number = 0;
+  while (current_node != NULL)
+    {
+      printf("%i\n", current_node->number);
+      current_node = current_node->next;
+      number++;
+    }
 
 /**
- * add_nodeint_end – new node will be added to linked list
- * @head: header pointer
- * @num: number of nodes
- * Return: new elements returned
- */
-listint_t *add_nodeint_end(listint_t **head, const int num)
-{
-	listint_t *new_node;
-	listint_t *current_node;
-
-	current_node = *head;
-
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->num = num;
-	new_node->next_node = NULL;
-
-	if (*head == NULL)
-		*head = new_node;
-	else
-	{
-		while (current_node->next_node != NULL)
-			current_node = current_node->next_node;
-		current_node->next_node = new_node;
-	}
-	return (new_node);
-}
-
-/**
- * free_listint – list to be freed
- * @head: freeing the pointers
- * Return: void function
+ * free_listint – sets free the linked list
+ * @head: pointer to be set free
+ * Return: void
  */
 void free_listint(listint_t *head)
 {
-	listint_t *current_node;
+  listint_t *current_node;
 
-	while (head != NULL)
-	{
-		current_node = head;
-		head = head->next_node;
-		free(current_node);
-	}
+  while (head != NULL)
+    {
+      Current_node = head;
+      head = head->next;
+      free(current_node);
+    }
 }
+
 
