baa7779ffbe358b34b2eec8ae12cc4e7ffcd3d38
= <=>
2023-06-14 05:49:14 -0700

    Task 9 solved

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,10 +2,10 @@
 two = 2
 def multiply_by_2(a_dictionary):
     multiplied_nums = a_dictionary.copy()
-    sorted_list = list(multipled_nums.keys())
+    sorted_list = list(multiplied_nums.keys())
 
     for initial in sorted_list:
-        multipled_nums[initial] *= two
+        multiplied_nums[initial] *= two
 
     return (multiplied_nums)
 
