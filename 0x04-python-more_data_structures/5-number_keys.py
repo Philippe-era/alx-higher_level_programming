dedfdc1d4d7a7d1208581de202aa365c3e651422
= <=>
2023-06-14 05:45:03 -0700

    TASK 5

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -3,7 +3,7 @@ def number_keys(a_dictionary):
     number_check = 0
     keys_listed = list(a_dictionary.keys())
 
-    for initial in listed_keys:
+    for initial in keys_listed:
         number_check += 1
 
     return (number_check)
