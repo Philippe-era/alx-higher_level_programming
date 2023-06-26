956e0569e1c803a834df3d7803b68ec72890735b
= <=>
2023-06-26 03:52:16 -0700

    fixed

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -13,7 +13,7 @@ def safe_print_list_integers(my_list=[], x=0):
     final = 0
     for initial in range(0, x):
         try:
-            print("{:d}".format(my_list[i]), end="")
+            print("{:d}".format(my_list[initial]), end="")
             final += 1
         except (ValueError, TypeError):
             continue
