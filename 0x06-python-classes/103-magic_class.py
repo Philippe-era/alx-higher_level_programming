23e564f555a8509b3701bfd7cbc2f03d053515db
= <=>
2023-06-27 06:24:52 -0700

    final

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -5,7 +5,7 @@ class MagicClass:
     
 
     def __init__(self, radius=0):
-        two = 2
+        
 
        """ Arg:
             radius (float or int): The radius of the new MagicClass.
@@ -16,8 +16,8 @@ class MagicClass:
         self.__radius = radius
 
     def area(self):
-        return (self.__radius ** two * math.pi)
+        return (self.__radius ** 2 * math.pi)
 
     def circumference(self):
-        return (two * math.pi * self.__radius)
+        return (2 * math.pi * self.__radius)
 
