9cd4e302f02edd70b35c22be8c152d63624c3674
= <=>
2023-09-12 05:59:56 -0700

    7 MULTI

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,10 +3,8 @@ const x = Math.floor(Number(process.argv[2]));
 if (isNaN(x)) {
   console.log('Missing number of occurrences');
 } else {
-	let initial = 0;
-	while (initial < x ) {
-	console.log('C is fun');
-	intial++;
-}
+  for (let initial = 0; initial < x; initial++) {
+    console.log('C is fun');
+  }
 }
 
