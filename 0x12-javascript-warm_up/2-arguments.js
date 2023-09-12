09a1b90556d6d9134332e0199a05c9485f89f9dc
= <=>
2023-09-12 05:55:48 -0700

    Arguement

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 const count_letters = process.argv.length;
-console.log(count_letters === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
+console.log(count_letters === 2 ? 'No argument' : count_letters === 3 ? 'Argument found' : 'Arguments found');
 
