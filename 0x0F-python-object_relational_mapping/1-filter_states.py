552a2c4d48a4eb92d7d3b07c9617249ead325a6b
= <=>
2023-09-17 04:32:27 -0700

    done i think

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-"""Returns the information from the database."""
-import sys
+"""all information from database will be returned """
+""" returns information on database based on conditions set """
 import MySQLdb
+import sys
+
 
 if __name__ == "__main__":
-    # connection to the database
-    database_create = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], database_create=sys.argv[3])
-    cursor_run = database_create.cursor()
-
-    # execute the query in order
-    cursor_run.execute("SELECT * FROM `states` ORDER BY `id`")
-    [print(state) for state in c.fetchall() if state[1][0] == "N"]
-
+    database_connect = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], database_connect=sys.argv[3], port=3306)
+    cursor_create = database_connect.cursor()
+    cursor_create.execute("""SELECT * FROM states WHERE name
+                LIKE BINARY 'N%' ORDER BY states.id""")
+    rows_check = cursor_create.fetchall()
+    for row in rows_check:
+        print(row)
+    cursor_create.close()
+    database_connect.close()
