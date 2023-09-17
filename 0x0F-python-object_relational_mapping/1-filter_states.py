1c73494f2438c9723646eda3f03f04e2b77d03ae
= <=>
2023-09-17 04:11:06 -0700

    FIEN

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""list all states in the database."""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    # information from database will be received 
+    # connection to MYSQL database
+    database_connect = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],      database_connect=sys.argv[3])
+    credentials = database_connect.cursor()
+
+    #execution of the mysql query
+    credentials.execute("SELECT * FROM `states` ORDER BY `id`")
+    [print(state) for state in credentials.fetchall() if state[1][0] == "N"]
+
+
