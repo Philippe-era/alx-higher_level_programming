457e4fe0a361cc832dd9d8673ec9ee51c1903bc8
= <=>
2023-09-17 05:07:15 -0700

    6

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -7,7 +7,7 @@ import sys
 
 if __name__ == "__main__":
     database_connect = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+                         passwd=sys.argv[2], database_connect=sys.argv[3], port=3306)
     cursor_connect = database_connect.cursor()
     cursor_create.execute("""SELECT cities.id, cities.name, states.name FROM
                 cities INNER JOIN states ON states.id=cities.state_id""")
